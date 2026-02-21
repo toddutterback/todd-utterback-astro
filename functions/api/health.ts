f052bf89ddf4cc95d083104f646a55bf8f134d7c
Todd Utterback <todd.j.utterback@gmail.com>
2026-02-20 22:13:14 -0500

    add health

---
+++ functions/api/health.ts
@@ -0,0 +1,6 @@
+export const onRequestGet: PagesFunction = async () => {
+	return new Response(JSON.stringify({ ok: true }), {
+		headers: { 'content-type': 'application/json; charset=utf-8' },
+	});
+};
+
